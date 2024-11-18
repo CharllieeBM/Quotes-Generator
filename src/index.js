01697442bf995e6f081c2ded09d09d4409628ee9
CharllieeBM <cburylb2@gmail.com>
2024-11-18 22:22:52 +0200

    modified html to add hints, added blink animation, and added a temporary message in between.

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ function generateQuote(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "d4ef035e3fbd4697b7a638t907f10o0c";
-  let prompt = `User instructions: Generate a parenting quote about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate a quote about parenting using ${instructionsInput.value} as keywords`;
   let context =
-    "You are an expert about parenting and you love to write quotes. Your task is to separate the sentences into two separate lines and to make sure to follow user instructions. Pleae sign '— SheCodes AI' inside a <strong> element at the end of the quote and put the signature in a new line.";
+    "Your task is to separate the sentences into two separate lines and to make sure to follow user instructions. Please sign '— SheCodes AI' inside a <strong> element at the end of the quote and put the signature in a new line.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="blink">⏳ Please wait, generating a quote now about ${instructionsInput.value}...</div>`;
 
   axios.get(apiUrl).then(displayQuote);
 }
